358f6ba8e865963f9812e53126891c7eccecdb22
onur-celik <onurcelik@me.com>
2022-10-26 17:21:52 +0300

    new version

--- index.js
+++ index.js
@@ -1,5 +1,36 @@
-function locst() {
-    return true;
-}
+const locst = {
+    isLocalStorageAvailable: false,
+    checkIfLocalStorageAvailable() {
+        return typeof localStorage === "object" ? true : false;
+    },
+    addItem(key, data) {
+        try {
+            localStorage.setItem(key, JSON.stringify(data));
+        } catch (err) {
+            throw err;
+        }
+        return true;
+    },
+    removeItem(key) {
+        try {
+            localStorage.removeItem(key);
+        } catch (err) {
+            throw err;
+        }
+        return true;
+    },
+    updateItem(key, newData) {
+        try {
+            const currentData = JSON.parse(localStorage.getItem(key));
+            localStorage.setItem(
+                key,
+                JSON.stringify({ ...currentData, newData })
+            );
+        } catch (err) {
+            throw err;
+        }
+        return true;
+    },
+};
 
 module.exports = locst;
